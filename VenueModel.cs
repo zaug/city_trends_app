8c3d92c2d1b5e2229e444e00048a5b87a03fcee6
David Teket <zaug@outlook.com>
2014-05-27 03:48:08 +0200

    Update VenueModel.cs

--- VenueModel.cs
+++ VenueModel.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 namespace City_Trends
 {
     // Módosítsuk az App.xaml.cs-t, hogy az új adatmodellt használja.
-    // Dokumentálva!
     public class VenueModel // Az adatszerkezet modellje.
     {
         public ReturnCode meta { get; set; }             // Viszatérési érték.
